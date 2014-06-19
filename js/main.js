af38521f6f3882c053474e824cf482ad7f6af9d8
ahodesuka <ahoka@lavabit.com>
2014-06-18 22:47:22 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var config =
+{
+    "sites":
+    [
+        { "url": "https://boards.4chan.org/jp/",                                    "text": "/jp/" },
+        { "url": "https://boards.4chan.org/a/",                                     "text": "/a/"  },
+        { "url": "https://boards.4chan.org/g/",                                     "text": "/g/"  },
+        { "url": "http://nibl.co.uk/bots.php",                                      "text": "XDCC" },
+        { "url": "https://sceneaccess.eu",                                          "text": "SCC"  },
+        { "url": "http://bakabt.me/",                                               "text": "BBT"  },
+        { "url": "http://anidb.net/perl-bin/animedb.pl?show=mylist&do.filter=1",    "text": "ADB"  },
+        { "url": "http://www.mangaupdates.com/mylist.html",                         "text": "MU"   },
+        { "url": "http://vgmdb.net/",                                               "text": "VGM"  },
+        { "url": "https://github.com/",                                             "text": "GH"   },
+        { "url": "http://www.last.fm/home",                                         "text": "l.fm" },
+        { "url": "http://what.cd/index.php",                                        "text": "W.CD" },
+    ],
+    "search":
+    {
+        "url"  : "https://duckduckgo.com",
+        "name" : "DuckDuckGo",
+        "query": "q"
+    }
+}, d = document;
+
+d.addEventListener("DOMContentLoaded", function()
+{
+    d.removeEventListener("DOMContentLoaded", arguments.callee, false);
+
+    for (var i = 0, MAX = config.sites.length; i < MAX; ++i)
+    {
+        var p = i / 12,
+            site = d.createElement("a");
+        site.href = config.sites[i].url;
+        site.textContent = config.sites[i].text;
+
+        if (i % 12 === 0)
+        {
+            var page = d.createElement("div");
+            page.id = "page" + p;
+
+            d.querySelector("#sites").appendChild(page);
+            // d.querySelector("#navigation ul").appendChild((d.createElement("li")));
+
+            if (p === 0)
+            {
+                // d.querySelector("#navigation ul li").classList.add("selected");
+                d.querySelector("#sites > div").classList.add("selected");
+            }
+        }
+
+        d.querySelector("#page" + Math.floor(p)).appendChild(site);
+    }
+
+    var form = d.querySelector("#search form"),
+        text = form.querySelector("input");
+    form.action      = config.search.url;
+    text.name        = config.search.query;
+    text.placeholder = config.search.name;
+    text.focus();
+});
